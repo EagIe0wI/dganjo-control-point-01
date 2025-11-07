f03fde8f2a7f0ff6997ba28006a8b3146057008d
EagIe0wO <anya230704@gmail.com>
2025-11-07 10:51:00 +0300

    added custom authorization loaded data drom cinema.json

--- authorization/models.py
+++ authorization/models.py
@@ -1,11 +1,82 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
 
-class User(models.Model):
-    fio = models.CharField(max_length=50, null=True, blank=True)
-    login = models.CharField(unique=True, max_length=50)
-    reg_data = models.DateTimeField(auto_now_add=True) #00-00-0000-00:00
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.role = 'admin'
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+
+    roles = [
+    ('admin', 'Администратор'),
+    ('manager', 'Менеджер'),
+    ('user', 'Пользователь')
+    ]
+
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    # is_admin = models.BooleanField(default=False)
+    role = models.CharField(max_length=10, choices=roles, default='user')
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["date_of_birth"]
 
     def __str__(self):
-        return f"{self.fio.title()}"
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        if self.role == 'admin' or self.role == 'manager':
+            return True
+        else:
+            return False
--- project/settings.py
+++ project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cinema',
+    'authorization',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
+AUTH_USER_MODEL = "authorization.MyUser"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
